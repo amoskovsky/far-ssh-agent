96329cc877051b6e38a5a22c1deef4c9ffeabf25
Anatoly Moskovsky <amoskovsky@gmail.com>
2012-02-04 12:27:40 +0200

    Added version resource (refs #352)

--- helper.py
+++ helper.py
@@ -1,3 +1,5 @@
+import re
+
 def getstatusoutput(cmd):
     """Return (status, output) of executing cmd in a shell."""
 
@@ -17,3 +19,15 @@ def git_version_string():
         # git not available/not a git repository/empty repository
         return ''
     return cmd_git[1]
+
+def git_version_to_numeric_version(s):
+    # v0.1.2[-3-...] -> 0,1,2,3
+    num_ver = "1, 0, 0, 0"
+    m = re.match(r'v(\d+)\.(\d+)\.(\d+)(?:-(\d+).*)?', s)
+    if (m):
+        v1, v2, v3, v4 = m.group(1), m.group(2), m.group(3), m.group(4)
+        if not v4: v4 = "0"
+        num_ver = "%s, %s, %s, %s" % (v1, v2, v3, v4)
+    return num_ver
+
+        
